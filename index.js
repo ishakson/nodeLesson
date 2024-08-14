0077d23f2c50a62b6b05c17bccfc417c1df62242
ishak <ishaksylmz7@gmail.com>
2024-08-14 07:26:45 +0300

    main

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
+const replaceTemplate = require("./modules/replaceTemplate");
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-  return output;
-};
+
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
-
+dataObj.map(el => {
+  el.slug = slugify(el.productName, { lower: true });
+})
 
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
+
+  const { pathname} = url.parse(req.url, true);
+
+  if (pathname.includes("/overview") || pathname === "/") {
     res.writeHead(200,{
       "Content-type": "text/html",
     })
@@ -33,9 +28,21 @@ const server = http.createServer((req, res) => {
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
     res.end(output);
 
-  } else if (pathName === "/product") {
-    res.end("hello product");
-  }else if(pathName === "/api"){
+  } else if (pathname.includes("/product")) {
+    const slug = pathname.replace('/product/', '');
+  if (!slug || slug === "/product") {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('<h1>There is no product name at url</h1>');
+    return;
+  }
+
+  res.writeHead(200, { 'Content-type': 'text/html' });
+  const product = dataObj.find(element => element.slug === slug);
+
+  
+  const output = replaceTemplate(tempProduct, product);
+  res.end(output);
+  }else if(pathname.includes("/api")){
 
     res.writeHead(200, {
       "Content-type": "application/json",
