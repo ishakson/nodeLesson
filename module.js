32dcc363df1ef37c38ea08acc11f3001439d1afb
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-08-19 19:03:26 +0000

    Main

---
+++ module.js
@@ -0,0 +1,11 @@
+// const calculator = require("./test-module-1.js");
+// const cal1 = new calculator();
+// console.log(cal1.add(1,2));
+
+// const calculator2 = require("./test-module-2.js");
+// console.log(calculator2.add(1,2));
+
+require(".//test-module-3.js")()
+require(".//test-module-3.js")()
+
+require(".//test-module-3.js")()
---
+++ test-module-3.js
@@ -0,0 +1,4 @@
+console.log("hello world from the module");
+module.exports = () => {
+    console.log("hello world");
+}
