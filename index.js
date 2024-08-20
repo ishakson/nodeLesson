d33e321232fa05396d14885e83d63d1a7074d1a7
ultrabot <72345759+ishakson@users.noreply.github.com>
2024-08-20 14:46:10 +0000

    Main

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const fs = require('fs');
+
 const superagent = require('superagent');
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) return console.log(err);
-  console.log(`Breed: ${data}`);
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((response) => {
-      console.log(response.body.message);
-
-      fs.writeFile('dog-img.txt', response.body.message, (err) => {
-        console.log('Random dog image saved to file.');
-      });
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject("I coudnt find that file");
+      }else {
+        resolve(data);
+      }
+      
     })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+  })
+}
+
+const writeFilePro= (file, data) =>{
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if(err) reject("i couldnt write that file");
+      resolve("File written")
+    })
+  })
+}
+
+const getDogPic = async () => {
+  try{
+  const data = await readFilePro(`${__dirname}/dog.txt`);
+  console.log(`Breed: ${data}`)
+  const response = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  console.log(response.body.message);
+ await  writeFilePro(`${__dirname}/dog-img.txt`, response.body.message);
+  console.log("File written");
+  }catch(err){
+    console.log(err);
+  }
+  
+}
+
+getDogPic()
