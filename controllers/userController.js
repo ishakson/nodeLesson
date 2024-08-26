8077ee0bab60110d861c3ed82268b3a82dcae8b9
ishak <ishaksylmz7@gmail.com>
2024-08-26 16:32:33 +0300

    main

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined! Please use /api/v1/users',
+    });
+  };
+  
+  exports.getUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined! Please use /api/v1/users/:id',
+    });
+  };
+  
+  exports.createUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined! Please use /api/v1/users',
+    });
+  };
+  
+  exports.updateUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined! Please use /api/v1/users/:id',
+    });
+  };
+  
+  exports.deleteUser = (req, res) => {
+    res.status(500).json({
+      status: 'error',
+      message: 'This route is not yet defined! Please use /api/v1/users/:id',
+    });
+  };
