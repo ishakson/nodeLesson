7de03b3ecdb8d8296f2f7264ba7b45ffac9c83d7
ishak <ishaksylmz7@gmail.com>
2024-09-08 08:50:14 +0300

    main

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
-console.log(process.env);
+
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
