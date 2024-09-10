d41f15e6f63895f32939f0a69328cdc15be09afb
ishak <ishaksylmz7@gmail.com>
2024-09-10 09:15:58 +0300

    main

--- server.js
+++ server.js
@@ -11,25 +11,6 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-
-const Tour = mongoose.model('Tour', tourSchema);
-
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
